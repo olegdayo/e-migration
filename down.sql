6602c065ab26c90fbca48bcfceae5458243f7d41
Mmmr7 <ol-sidorenkov@mail.ru>
2022-11-04 19:05:44 +0300

    rm

--- down.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS olympics;
-
-drop table if exists olympics.Results;
-drop table if exists olympics.Players;
-drop table if exists olympics.Events;
-drop table if exists olympics.Olympics;
-drop table if exists olympics.Countries;
